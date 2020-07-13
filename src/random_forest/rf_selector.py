e7b2058be4d9da2bad12e1e7b21d313a04b9885b
Timur Kasimov <timurcat7@gmail.com>
2020-07-13 21:02:19 +0700

    Removed unused params. Plotting function shows importances of all features.

--- src/random_forest/rf_selector.py
+++ src/random_forest/rf_selector.py
@@ -1,26 +1,39 @@
-from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 
 class RFSelector:
-    def __init__(self, estimator_params: dict, k_features: int, x_train, y_train, scoring: callable = accuracy_score,
-                 test_size: float = 0.2, random_state: int = 1) -> None:
+    def __init__(self, estimator_params: dict, k_features: int, x_train, y_train, *args,
+                 scoring: callable = accuracy_score, **kwargs) -> None:
         self.__k_features = k_features
         self.__x_train = x_train
         self.__y_train = y_train
         self.__scoring = scoring
-        self.__test_size = test_size
-        self.__random_state = random_state
         self.__forest = RandomForestClassifier(**estimator_params)
+        self.__importances = None
+        self.__indices = None
 
-    def plot(self):
-        pass
+    def plot(self, feat_labels: np.ndarray, fig_size, *args, title: str = 'Importances',
+             color: str = 'green', align: str = 'center', **kwargs) -> plt.figure:
+        if self.__importances is None:
+            self.select()
 
-    def select(self):
-        self.__forest.fit(self.__x_train, self.__y_train)
-        importances = self.__forest.feature_importances_
-        indices = np.argsort(importances)[::-1]
+        fig, ax = plt.subplots(figsize=fig_size)
+        fig.tight_layout()
 
-        return indices[:self.__k_features]
+        ax.set_title(title)
+        # ax.set_xticks(range(self.__x_train.shape[1]), feat_labels[self.__indices])
+        # ax.set_xlim([-1, self.__x_train.shape[1]])
+        ax.bar(range(self.__x_train.shape[1]), self.__importances[self.__indices], color=color, align=align)
+
+        return fig
+
+    def select(self) -> np.ndarray:
+        if self.__importances is None:
+            self.__forest.fit(self.__x_train, self.__y_train)
+            self.__importances = self.__forest.feature_importances_
+            self.__indices = np.argsort(self.__importances)[::-1]
+
+        return self.__indices[:self.__k_features]
