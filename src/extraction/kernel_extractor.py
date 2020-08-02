7ce3b16a7c505e0b55e5e173e256cf032b25a5a8
SN4KEBYTE <timurcat7@gmail.com>
2020-08-02 21:08:04 +0700

    Init commit.

---
+++ src/extraction/kernel_extractor.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Callable
+
+import numpy as np
+from nptyping import Number
+from nptyping.ndarray import NDArray
+
+from src.extraction.base_extractor import BaseExtractor
+
+
+class KernelExtractor(BaseExtractor, ABC):
+    def __init__(self, n_components: int, gamma: float = 1.0, kernel: str = 'rbf') -> None:
+        super(KernelExtractor, self).__init__(n_components)
+        self._gamma = gamma
+        self._kernel_func: Callable = self.__get_kernel(kernel)
+
+    @abstractmethod
+    def fit(self):
+        pass
+
+    @abstractmethod
+    def transform(self) -> NDArray[Number]:
+        pass
+
+    @abstractmethod
+    def fit_transform(self) -> NDArray[Number]:
+        pass
+
+    def __get_kernel(self, kernel: str) -> Callable:
+        kernels: Dict = {'rbf': self.__rbf}
+
+        return kernels[kernel]
+
+    def __rbf(self, mat_dists: NDArray[Number]) -> NDArray[Number]:
+        return np.exp(-self._gamma * mat_dists)
---
+++ src/extraction/linear_extractor.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
+import numpy as np
+from nptyping import Number
+from nptyping.ndarray import NDArray
+
+from src.extraction.base_extractor import BaseExtractor
+
+
+class LinearExtractor(BaseExtractor, ABC):
+    def __init__(self, n_components: int) -> None:
+        super(LinearExtractor, self).__init__(n_components)
+        self._w: NDArray[Number] or None = None
+        self._variance_explained: List or None = None
+
+    @abstractmethod
+    def fit(self):
+        pass
+
+    def transform(self, x_train: NDArray[Number]) -> NDArray[Number]:
+        if self._w is None:
+            raise RuntimeError('Projection matrix is not calculated. Please use fit method first, or fit_transform.')
+
+        return x_train.dot(self._w)
+
+    @abstractmethod
+    def fit_transform(self) -> NDArray[Number]:
+        pass
+
+    def _calculate_projection_matrix(self, eigen_vals: NDArray[Number], eigen_vecs: NDArray[Number]) -> None:
+        eigen_pairs: List[Tuple[Number, NDArray[Number]]] = sorted(
+            [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))],
+            key=lambda k: k[0], reverse=True)
+
+        new_features: List[NDArray[Number]] = [eigen_pairs[i][1][:, np.newaxis].real for i in range(self._n_components)]
+        self._w = np.hstack(new_features)
+
+    def _calculate_variance_explained(self, eigen_vals: NDArray[Number]) -> None:
+        eigen_sum = np.sum(eigen_vals)
+        self._variance_explained = [val / eigen_sum for val in eigen_vals]
+
+    @property
+    def projection_matrix(self) -> NDArray[Number]:
+        return self._w
+
+    @property
+    def variance_explained(self) -> NDArray[Number]:
+        return self._variance_explained
