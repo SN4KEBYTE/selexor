060b0afc678dd1498fca2238195af2cffc4a7d4b
SN4KEBYTE <timurcat7@gmail.com>
2020-08-10 20:41:38 +0700

    Added custom types for random_state and test_size params.

--- selexor/sbs/types.py
+++ selexor/sbs/types.py
@@ -1,8 +1,11 @@
-from typing import Callable, List, Tuple
+from typing import Callable, List, Tuple, Optional, Union
 
+import numpy as np
 from nptyping import Number
 from nptyping.ndarray import NDArray
 
 Subset = Tuple[int, ...]
 FeatureSet = Tuple[List[Subset], float]
 AccuracyScore = Callable[[NDArray[Number], NDArray[Number]], float]
+RandomState = Optional[Union[int, np.random.RandomState]]
+Proportion = Optional[Union[float, int]]
