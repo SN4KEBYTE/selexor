b841714586a84351278b62fe2aad3c21e3a9d792
SN4KEBYTE <timurcat7@gmail.com>
2020-08-12 03:53:55 +0700

    Removed duplicated type description.

--- selexor/sbs/types.py
+++ selexor/sbs/types.py
@@ -6,6 +6,5 @@ from nptyping.ndarray import NDArray
 
 Subset = Tuple[int, ...]
 FeatureSet = Tuple[List[Subset], float]
-AccuracyScore = Callable[[NDArray[Number], NDArray[Number]], float]
 RandomState = Optional[Union[int, np.random.RandomState]]
 Proportion = Optional[Union[float, int]]
